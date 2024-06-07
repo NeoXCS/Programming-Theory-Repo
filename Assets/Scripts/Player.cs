22a24a3aa6fe81250ba7ccd9c449f9c99574e2cd
NeoXCS <neomaster3x@gmail.com>
2024-06-07 17:00:44 -0400

    Added Shooting Mechanics
    - Player can shoot
    - Shooting is animated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,11 +8,14 @@ public class Player : MonoBehaviour
    private float horizontalInput;
    [SerializeField] private float speed = 15.0f;
    [SerializeField] private float xRange = 25.0f;
+   [SerializeField] private GameObject projectilePrefab;
+   private bool canShoot = true;
+   private Animator playerAnim;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +31,11 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
         }
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
 
         PlayerMove(); //abstraction
     }
@@ -35,5 +44,29 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed* Time.deltaTime);
+    }
+
+    void Shoot()
+    {
+        if(canShoot)
+        {
+            Vector3 projectileSpawn = new Vector3(transform.position.x + 0.9f, projectilePrefab.transform.position.y, projectilePrefab.transform.position.z);
+        
+            Instantiate(projectilePrefab, projectileSpawn, projectilePrefab.transform.rotation);
+
+            playerAnim.SetTrigger("Shoot");
+
+            canShoot = false;
+
+            StartCoroutine(ShootDelay());
+        }
+        
+
+    }
+
+    IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(0.8f);
+        canShoot = true;
     }
 }
